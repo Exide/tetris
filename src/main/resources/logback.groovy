ae3cabc2a718f9ba6363e75aea24ed5b7084d0a4
Ryan Zander <exide@hotmail.com>
2016-04-09 18:55:55 -0700

    Changed default log level to INFO.

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,10 +1,7 @@
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.ConsoleAppender
-
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%date{E h:mm:ss.SSS} %logger{1}:%line [%level]: %msg\n"
     }
 }
 
-root(DEBUG, ["CONSOLE"])
+root(INFO, ["CONSOLE"])
